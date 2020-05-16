8419a6c833116277234a049024c2a8daf7061f58
abi bayu pamungkas <55426650+abibayu@users.noreply.github.com>
2020-05-16 10:47:21 +0700

    Create 11Latihan2.php

---
+++ 11Latihan2.php
@@ -0,0 +1,10 @@
+<? 
+mysql_connect("localhost","root","database"); 
+$dbname="lat_dbase"; 
+$cek=mysql_query("CREATE DATABASE $dbname") or die(Couldn't Create Database);
+$dbname");  
+if($cek);
+{  
+echo "Database $dbname berhasil dibuat"; 
+} 
+?>
